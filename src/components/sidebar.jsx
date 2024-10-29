26a4ef9c3b9599803692a17f146c9c14a9091950
evanlin-dev <evanlin2912@gmail.com>
2024-10-29 10:29:30 -0400

    fix

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Sidebar.css';
+import './sidebar.css';
 import { Box, Typography, List, ListItem, ListItemButton, ListItemText } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
